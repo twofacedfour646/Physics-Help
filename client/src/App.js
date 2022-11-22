02d8da10e4b7c86c43965220a89de67f705261b3
Jumping Biscuit <>
2022-11-22 14:42:52 +0000

    Started designing landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Landing from "./pages/Landing.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="app">
-      
+			<Navbar />
+      <div className="container">
+				<Landing />
+			</div>
     </div>
   );
 }
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Landing = () => {
     return (
         <div className="landing-page">
-            
+            <div class="jumbotron">
+						  <h1 class="display-4">Welcome to Physics Help!</h1>
+						  <p class="lead">This is an application that makes it easy, fun, and rewarding to learn about physics!</p>
+						  <hr class="my-4"/>
+						  <p>Test out your skills and knowledge on hundreds of our real life problems!</p>
+						  <p class="lead">
+						    <a class="btn btn-primary btn-lg" href="#" role="button">Start Learning</a>
+						  </p>
+						</div>
         </div>
     )
 }
